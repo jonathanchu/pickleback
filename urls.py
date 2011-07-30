cbafbaf6bcbfe03dd7362c0dc0b998c277af2de5
Jonathan Chu <jonathan.chu@me.com>
2011-07-30 03:20:50 -0400

    Add 404, 500, base, home templates with basic styling and format. jQuery min and url routing for homepage

--- urls.py
+++ urls.py
@@ -1,17 +1,17 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import direct_to_template
 from django.contrib import admin
 admin.autodiscover()
 
+import settings
+
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'pickleback.views.home', name='home'),
-    # url(r'^pickleback/', include('pickleback.foo.urls')),
+    # home
+    url(r'^$', direct_to_template, { "template": "home.html", }, name="home"),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    # admin
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 )
 
 if settings.DEBUG:
