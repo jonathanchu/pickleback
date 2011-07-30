85db2625da530dcc8210b90cb176f56f6f2c998e
Jonathan Chu <jonathan.chu@me.com>
2011-07-30 16:40:04 -0400

    Add some more fields to packages for PyPI and repo identification

--- apps/packages/admin.py
+++ apps/packages/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from pickleback.apps.packages.models import Package
 
 class PackageAdmin(admin.ModelAdmin):
-    list_display = ('name', 'url', 'description')
+    list_display = ('name', 'url', 'description', 'canonical')
 
 admin.site.register(Package, PackageAdmin)
